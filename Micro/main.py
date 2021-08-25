3bd6b2ba814a551f115d41b60f6c9e00ea84f006
Yordan Rodriguez <yerp35@gmail.com>
2021-08-25 18:34:26 -0500

    Lectura de senson y configuracion de timer

--- Micro/main.py
+++ Micro/main.py
@@ -1,6 +1,16 @@
-from machine import network
-print ("Leer datos")
+import network
+from machine import Timer,ADC, Pin
+from umqtt.simple import MQTTClient
+import ubinascii
+dato =['a','b','c']
+analogo = ADC(0)
+TOPIC = "prueba"
+led = Pin(2,Pin.OUT)
+temp = Timer(-1)
+bandera=0
+led.value(1)
 def conectar ():
+	global dato
 	archivo = open('wifi.txt','r')
 	dato  = archivo.read().split(',')
 	archivo.close()
@@ -13,4 +23,30 @@ def conectar ():
 			pass
 		print('network config:', sta_if.ifconfig())
 
+def enviar():
+	pass
+
+def funcion(t):
+	led.off()
+
 conectar()
+CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+cliente = MQTTClient(CLIENT_ID, dato[2])
+cliente.set_callback(enviar) ##Define una funcion que se activa cada interaccion 
+cliente.connect()
+cliente.subscribe(TOPIC)
+print("Connected to server %s, subscribed to topic: %s" % (dato[2], TOPIC))
+print("Configurando temporizador")
+temp.init(mode=Timer.PERIODIC,period=2000,callback=funcion)
+print("Listo")
+try:
+	while 1:
+		# micropython.mem_info()
+		if (led.value()==0):
+			entrada = analogo.read()
+			print(entrada)
+			cliente.publish(topic=TOPIC,msg=str(entrada))
+			bandera = 0;
+			led.on()
+finally:
+	cliente.disconnect()
